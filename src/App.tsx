1ce133262bfb61e635674f1b618d38c239497242
young2z1 <fftizlk0@gmail.com>
2026-05-20 15:39:19 +0900

    Feat: 환전 화면 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,6 @@ import Login from './pages/Login';
 import PasswordRecovery from './pages/PasswordRecovery';
 import Signup from './pages/Signup';
 import GoogleSignupInfo from './pages/GoogleSignupInfo';
-import HomePage from './pages/HomePage';
-import ChargePage from './pages/ChargePage';
-import AddAccountPage from './pages/AddAccountPage';
-import AutoDebitAuthPage from './pages/AutoDebitAuthPage';
-import AccountRegisteredPage from './pages/AccountRegisteredPage';
 
 function App() {
   return (
@@ -17,10 +12,6 @@ function App() {
       <Route path="/password-recovery" element={<PasswordRecovery />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/google-signup-info" element={<GoogleSignupInfo />} />
-      <Route path="/charge" element={<ChargePage />} />
-      <Route path="/charge/add-account" element={<AddAccountPage />} />
-      <Route path="/charge/auto-debit" element={<AutoDebitAuthPage />} />
-      <Route path="/charge/account-registered" element={<AccountRegisteredPage />} />
     </Routes>
   );
 }
