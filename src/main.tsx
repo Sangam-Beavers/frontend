139aacb7263d79104ef74efbf08a9e5874a7b0b2
Choi Seungmin <blanc_91@icloud.com>
2026-06-11 14:23:14 +0900

    Refactor: main.tsx i18n import를 @/ 절대경로로 통일

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
 import './index.css';
-import './i18n';
+import '@/i18n';
 
 const queryClient = new QueryClient({
   defaultOptions: {
